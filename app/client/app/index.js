1faa36edafabe7366db663720257976d8c55dfa0
kyselov dmytro <dkiselov.ua@gmail.com>
2017-01-06 14:41:16 +0200

    add

--- app/client/app/index.js
+++ app/client/app/index.js
@@ -2,7 +2,7 @@ let angular=require('angular');
 const ngModule=angular.module('app',[]);
 require('./directives')(ngModule);
 require('./agcontrollers/index')(ngModule);  
-console.log("Hello app787"); 
+console.log("Hello app world"); 
 
 
 
